8765b0c8c5bf8e02caec17194ab6d75df23cdcaf
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-13 17:15:42 +0000

    first commit

---
+++ lib/news_collator.rb
@@ -0,0 +1,6 @@
+require "news_collator/version"
+
+module NewsCollator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/news_collator/version.rb
@@ -0,0 +1,3 @@
+module NewsCollator
+  VERSION = "0.1.0"
+end
