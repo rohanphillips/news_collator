73330a7bfb182fea899d115cf2863599c3f9c743
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-15 13:30:45 -0500

    Basic CLI menu operational

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,6 +2,18 @@ require 'pry'
 
 class CLI
   def run
-    puts "CLI Menu"
+    menu_selection = 0
+    puts "Welcome to News Collator"
+    while menu_selection >= 0 && menu_selection < 3
+      puts "What would you like to do? (Enter number associated with you selection)"
+      puts "1. Scrape New Stories"
+      puts "2. Retrieve information relating to stories"
+      puts "3. Exit"
+      menu_selection = gets.chomp.to_i
+      if menu_selection == 0 || menu_selection > 3
+          menu_selection = 0
+          puts "Please enter a valid selection"
+      end
+    end
   end
 end
