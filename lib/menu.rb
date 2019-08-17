9ec157c1d77dfa9e89533506ef4f4e7856bc51bf
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-16 22:56:54 -0500

    Added ascii art to the exit message

--- lib/menu.rb
+++ lib/menu.rb
@@ -22,7 +22,32 @@ class MenuController
         when 2
           article_info_list
         when 3
-          puts "You rocked it!  Goodbye!"
+          puts ""
+          puts " "
+          puts "                                                                   ddddddddbbbbbbbb                                                        "
+          puts "        GGGGGGGGGGGGG                                              d::::::db::::::b                                                    !!! "
+          puts "     GGG::::::::::::G                                              d::::::db::::::b                                                   !!:!!"
+          puts "   GG:::::::::::::::G                                              d::::::db::::::b                                                   !:::!"
+          puts "  G:::::GGGGGGGG::::G                                              d:::::d  b:::::b                                                   !:::!"
+          puts " G:::::G       GGGGGG   ooooooooooo      ooooooooooo       ddddddddd:::::d  b:::::bbbbbbbbb yyyyyyy           yyyyyyy eeeeeeeeeeee    !:::!"
+          puts "G:::::G               oo:::::::::::oo  oo:::::::::::oo   dd::::::::::::::d  b::::::::::::::bby:::::y         y:::::yee::::::::::::ee  !:::!"
+          puts "G:::::G              o:::::::::::::::oo:::::::::::::::o d::::::::::::::::d  b::::::::::::::::by:::::y       y:::::ye::::::eeeee:::::ee!:::!"
+          puts "G:::::G    GGGGGGGGGGo:::::ooooo:::::oo:::::ooooo:::::od:::::::ddddd:::::d  b:::::bbbbb:::::::by:::::y     y:::::ye::::::e     e:::::e!:::!"
+          puts "G:::::G    G::::::::Go::::o     o::::oo::::o     o::::od::::::d    d:::::d  b:::::b    b::::::b y:::::y   y:::::y e:::::::eeeee::::::e!:::!"
+          puts "G:::::G    GGGGG::::Go::::o     o::::oo::::o     o::::od:::::d     d:::::d  b:::::b     b:::::b  y:::::y y:::::y  e:::::::::::::::::e !:::!"
+          puts "G:::::G        G::::Go::::o     o::::oo::::o     o::::od:::::d     d:::::d  b:::::b     b:::::b   y:::::y:::::y   e::::::eeeeeeeeeee  !!:!!"
+          puts " G:::::G       G::::Go::::o     o::::oo::::o     o::::od:::::d     d:::::d  b:::::b     b:::::b    y:::::::::y    e:::::::e            !!! "
+          puts "  G:::::GGGGGGGG::::Go:::::ooooo:::::oo:::::ooooo:::::od::::::ddddd::::::dd b:::::bbbbbb::::::b     y:::::::y     e::::::::e               "
+          puts "   GG:::::::::::::::Go:::::::::::::::oo:::::::::::::::o d:::::::::::::::::d b::::::::::::::::b       y:::::y       e::::::::eeeeeeee   !!! "
+          puts "     GGG::::::GGG:::G oo:::::::::::oo  oo:::::::::::oo   d:::::::::ddd::::d b:::::::::::::::b       y:::::y         ee:::::::::::::e  !!:!!"
+          puts "        GGGGGG   GGGG   ooooooooooo      ooooooooooo      ddddddddd   ddddd bbbbbbbbbbbbbbbb       y:::::y            eeeeeeeeeeeeee   !!! "
+          puts "                                                                                                  y:::::y                                  "
+          puts "                                                                                                 y:::::y                                   "
+          puts "                                                                                                y:::::y                                    "
+          puts "                                                                                               y:::::y                                     "
+          puts "                                                                                              yyyyyyy                                      "
+          puts "                                                                                                                                           "
+          puts "                                                                                                                                           "
       end
     end
   end
@@ -81,7 +106,6 @@ class MenuController
         when 4
           puts "What Keyword would you like to search for?"
           keyword = gets.chomp.downcase
-
           collection = Article.all.select{|article| (article.website.name == @website.name && article.headline.downcase.include?(keyword) == true)
 
           }
